0fa1ac72deec0516153f0568191ec8fcbc44edc6
AziAzi22 <aziazi22t@gmail.com>
2026-01-30 16:50:55 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,30 @@
+// 1 - bo'lim 
+
+// 1 - masala 
+
+// function myFunc(variable:any):any  {
+//     return variable
+// }
+
+// console.log(myFunc(1));
+// console.log(myFunc("hello"));
+// console.log(myFunc(true));
+// console.log(myFunc({a:1, b:2, c:3}));
+// console.log(myFunc([1,2,3]));
+
+// generic function
+
+// function myFunc<T>(variable:T):T {
+//     return variable
+// }
+
+// console.log(myFunc(1));
+// console.log(myFunc("hello"));
+// console.log(myFunc(true));
+
+
+
+// 2 - masala 
+
+
+
